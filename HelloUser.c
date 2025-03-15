801e7152c00aa171222cac9173e362500a92f197
UVM10 <mn4546615@gmail.com>
2025-03-15 20:58:35 +0000

    [DevEnv] Some changes

--- HelloUser.c
+++ HelloUser.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(int argc[], char* argv[])
 {
-	printf("Hello guys!, You can use a simple calculator here!");
+	printf("Hello guys!, You can use a simple todolist program here!");
 	return 0;
 }
