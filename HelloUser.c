58688476f981c88e81b14a8ba9b015c508797fba
UVM10 <mn4546615@gmail.com>
2025-04-08 14:03:23 +0000

    [ToDoList v1] Update 4_8

--- HelloUser.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(int argc[], char* argv[])
-{
-	printf("Hello guys!, You can use a simple todolist program here!");
-	return 0;
-}
---
+++ utils/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p build
+
+gcc -o "build/ToDoList" Main.c
